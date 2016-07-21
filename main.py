270db1384202db6cb03be2d5d153a346dff6bc0d
Cadel Watson <cadel@cadelwatson.com>
2016-07-21 18:59:47 +1000

    Remove old rule loading code

--- main.py
+++ main.py
@@ -60,43 +60,6 @@ def expand_rule(rule):
 
     return rules
 
-def load_rules(filename):
-    '''Loads a list of rules, in the following format:
-
-    [label1]
-    x:y
-    z:a
-
-    [label2]
-    b:c
-
-    which would become:
-
-    {'label1': (x, y) ..., 'label2': (b,c)}'''
-
-    # Read stripped, non-blank lines into list
-    with open(filename, 'r') as f:
-        lines = [line.strip() for line in f.readlines() if line.strip() != '']
-
-    # Iterate through list, splitting on labels
-    rules_list = []
-    current_rule = [lines[0][1:-1]]
-
-    for item in lines[1:]:
-        if item[0] == '[':
-            rules_list.append(current_rule)
-            current_rule = [item[1:-1]]
-        else:
-            current_rule.append(tuple(item.split(':')))
-    rules_list.append(current_rule)
-
-    # Convert rule list to dictionary
-    rules = {}
-    for rule in rules_list:
-        rules[rule[0]] = rule[1:]
-
-    return rules
-
 def apply_rule(word, rule):
     '''Applies rule in the form (target, replacement, environment) to the word.
     Environment is in the form of string like 'a_c', where the underscore is
