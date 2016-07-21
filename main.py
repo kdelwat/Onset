5457c8dc3e2a3c9255dce023ce8723e86ca60688
Cadel Watson <cadel@cadelwatson.com>
2016-07-21 17:47:55 +1000

    Move rules to their own file, because it's going to get big

--- main.py
+++ main.py
@@ -3,70 +3,8 @@ import re
 from itertools import takewhile
 from pprint import pprint
 
+import rules
 from table import Table
-
-PULMONIC = Table('pulmonic.csv')
-
-def rule(f):
-    '''A decorator for all rule functions which checks to ensure rules are
-    applicable to current inventory.'''
-    def wrapper(*args, **kwargs):
-        rules, representation = f(*args, **kwargs)
-
-        # If the length of the rules list is 0, there are no applicable rules, so
-        # return False.
-        if len(rules) == 0:
-            return False, None, None
-        else:
-            return True, rules, representation
-
-    return wrapper
-
-@rule
-def sonorization(inventory):
-    '''Implements the sonorization, or voicing, sound change, in which plosives are
-    converted to their voiced equivalent.'''
-
-    # List unvoiced plosives and their voiced equivalents
-    candidates = ['p', 't', 'ʈ', 'c', 'k', 'q']
-    targets = ['b', 'd', 'ɖ', 'ɟ', 'g', 'ɢ']
-
-    available_environments = ['^_', 'V_V']
-
-    environments = [random.choice(available_environments)] * len(candidates)
-
-    # Zip together the candidates, targets, and environments into a list of rules.
-    # Include only those rules which are relevant to the given inventory.
-    rules = [rule for rule in zip(candidates, targets, environments) if rule[0] in inventory]
-
-    representation = 'Sonorization: [unvoiced plosive]/[voiced plosive]/'+ environments[0]
-
-    return rules, representation
-
-@rule
-def degemination(inventory):
-    '''Implements the degemination sound change, in which doubled plosives are
-    converted to singular.'''
-
-    candidates = []
-    targets = []
-
-    for phoneme in PULMONIC['plosive']:
-        candidates.append(phoneme + phoneme)
-        candidates.append(phoneme + phoneme + 'ʰ')
-        targets.append(phoneme)
-        targets.append(phoneme + 'ʰ')
-
-    available_environments = ['^_', 'V_V']
-
-    environments = [random.choice(available_environments)] * len(candidates)
-
-    # Zip together the candidates, targets, and environments into a list of rules.
-    rules = [rule for rule in zip(candidates, targets, environments)]
-
-    representation = 'Degemination: [geminated plosive]/[plosive]/'+ environments[0]
-
-    return rules, representation
 
 def load_rules(filename):
     '''Loads a list of rules, in the following format:
@@ -126,10 +64,10 @@ def apply_rule(word, rule):
     return re.sub(target_string, replacement_string, word)
 
 def main():
-    rules = load_rules('rules.txt')
+    #rules = load_rules('rules.txt')
     inventory = Table('pulmonicinventory.csv')
-    pprint(sonorization(inventory))
-    pprint(degemination(inventory))
+    pprint(rules.sonorization(inventory))
+    pprint(rules.degemination(inventory))
 
 if __name__ == '__main__':
     main()
---
+++ rules.py
@@ -0,0 +1,66 @@
+import random
+
+from table import Table
+
+PULMONIC = Table('pulmonic.csv')
+
+def rule(f):
+    '''A decorator for all rule functions which checks to ensure rules are
+    applicable to current inventory.'''
+    def wrapper(*args, **kwargs):
+        rules, representation = f(*args, **kwargs)
+
+        # If the length of the rules list is 0, there are no applicable rules, so
+        # return False.
+        if len(rules) == 0:
+            return False, None, None
+        else:
+            return True, rules, representation
+
+    return wrapper
+
+@rule
+def sonorization(inventory):
+    '''Implements the sonorization, or voicing, sound change, in which plosives are
+    converted to their voiced equivalent.'''
+
+    # List unvoiced plosives and their voiced equivalents
+    candidates = ['p', 't', 'ʈ', 'c', 'k', 'q']
+    targets = ['b', 'd', 'ɖ', 'ɟ', 'g', 'ɢ']
+
+    available_environments = ['^_', 'V_V']
+
+    environments = [random.choice(available_environments)] * len(candidates)
+
+    # Zip together the candidates, targets, and environments into a list of rules.
+    # Include only those rules which are relevant to the given inventory.
+    rules = [rule for rule in zip(candidates, targets, environments) if rule[0] in inventory]
+
+    representation = 'Sonorization: [unvoiced plosive]/[voiced plosive]/'+ environments[0]
+
+    return rules, representation
+
+@rule
+def degemination(inventory):
+    '''Implements the degemination sound change, in which doubled plosives are
+    converted to singular.'''
+
+    candidates = []
+    targets = []
+
+    for phoneme in PULMONIC['plosive']:
+        candidates.append(phoneme + phoneme)
+        candidates.append(phoneme + phoneme + 'ʰ')
+        targets.append(phoneme)
+        targets.append(phoneme + 'ʰ')
+
+    available_environments = ['^_', 'V_V']
+
+    environments = [random.choice(available_environments)] * len(candidates)
+
+    # Zip together the candidates, targets, and environments into a list of rules.
+    rules = [rule for rule in zip(candidates, targets, environments)]
+
+    representation = 'Degemination: [geminated plosive]/[plosive]/'+ environments[0]
+
+    return rules, representation
