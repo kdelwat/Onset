0f473134c7b0d9e0ec83c0a3575ac8f4f44c184a
Cadel Watson <cadel@cadelwatson.com>
2016-07-24 12:46:02 +1000

    Add transcription input to web app

--- app/views.py
+++ app/views.py
@@ -9,15 +9,26 @@ def index():
     return render_template('index.html')
 
 
+def format_transcriptions(transcriptions):
+    '''Split the raw string of transcriptions into
+    the correct tuple rules.'''
+    return [(pair.split(':')[0], pair.split(':')[1]) for pair in transcriptions.split('\n')]
+
+
 @app.route('/evolve', methods=['POST'])
 def evolver():
     words = request.form['words'].split()
+
+    try:
+        transcriptions = format_transcriptions(request.form['transcriptions'])
+    except IndexError:
+        return jsonify({'error': 'Error: Transcription seperator must be a colon'})
 
     try:
         generations = int(request.form['generations'])
     except ValueError:
         return jsonify({'error': 'Error: Generations must be an integer'})
 
-    rules, words = evolve.evolve(words, generations)
+    rules, words = evolve.evolve(words, generations, transcriptions)
 
     return jsonify({'rules': rules, 'words': words, 'error': 0})
