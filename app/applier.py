2ef0571e5468ac72f712a69180fa5dc18652e8d7
Cadel Watson <cadel@cadelwatson.com>
2016-10-31 17:42:44 +1100

    Implement rule filtering by phoneme.

--- app/applier.py
+++ app/applier.py
@@ -14,15 +14,24 @@ words = ['potato', 'tobado', 'tabasco']
 def choose_rule(words, rules):
     '''Returns a sound change rule from rules applicable to the given word list.'''
     filtered_rules = filter_rules_by_phonemes(words, rules)
-    filtered_rules = filter_rules_by_environments(words, filtered_rules)
+    # filtered_rules = filter_rules_by_environments(words, filtered_rules)
 
     # selected_rule = random.choice(filtered_rules)
+
+def intersecting(set_1, set_2):
+    '''Return true if the intersection of the two sets isn't empty, false
+otherwise.
+    '''
+    return (len(set_1.intersection(set_2)) != 0)
 
 def filter_rules_by_phonemes(words, rules):
     '''Returns a list of rules which contain phonemes that are present in the given
 word list.
     '''
-    pass
+    word_phonemes = set(''.join(words))
+
+    return [rule for rule in rules if intersecting(word_phonemes,
+                                                   set(rule.changes.keys()))]
 
 def filter_rules_by_environments(words, rules):
     '''Returns a list of rules which apply to at least one word in the given word
