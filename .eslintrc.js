aa65a1c5577d91998a0452057a262fe2fb37475b
Cadel Watson <cadel@cadelwatson.com>
2016-11-20 13:45:28 +1100

    Stop ESLint complaining about localStorage

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,8 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-console': process.env.NODE_ENV === 'production' ? 2 : 0
+  },
+  'env': {
+    'browser': true
   }
 }
