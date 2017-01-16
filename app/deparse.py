9258bdf23f0019826c007cdf63ed956ad178ddbd
Cadel Watson <cadel@cadelwatson.com>
2017-01-16 15:17:08 +1100

    Add segment conversion to feature string

---
+++ app/deparse.py
@@ -0,0 +1,47 @@
+feature_order = ['syllabic',
+                 'stress',
+                 'long',
+                 'consonantal',
+                 'sonorant',
+                 'continuant',
+                 'delayedrelease',
+                 'approximant',
+                 'tap',
+                 'trill',
+                 'nasal',
+                 'voice',
+                 'spreadglottis',
+                 'constrictedglottis',
+                 'labial',
+                 'round',
+                 'labiodental',
+                 'coronal',
+                 'anterior',
+                 'distributed',
+                 'strident',
+                 'lateral',
+                 'dorsal',
+                 'high',
+                 'low',
+                 'front',
+                 'back',
+                 'tense']
+
+
+def feature_string(segment):
+    '''Convert a Segment object into a feature string.'''
+    features = []
+
+    for feature in feature_order:
+        if feature in segment.positive:
+            features.append('+')
+        elif feature in segment.negative:
+            features.append('-')
+        else:
+            features.append('0')
+
+    return ''.join(features)
+
+
+def deparse_words(words, segments, diacritics):
+    pass
