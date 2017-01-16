cc2c9ab8a2d888cc0a74b177fccd6f9fa00e14e0
Cadel Watson <cadel@cadelwatson.com>
2017-01-16 16:12:59 +1100

    Add segment matching to deparse

--- app/deparse.py
+++ app/deparse.py
@@ -1,3 +1,5 @@
+from Levenshtein import distance
+
 feature_order = ['syllabic',
                  'stress',
                  'long',
@@ -41,6 +43,33 @@ def feature_string(segment):
             features.append('0')
 
     return ''.join(features)
+
+
+def best_segment_match(target_feature_string, all_feature_strings):
+    '''Returns the best match for the segment denoted by the target feature string,
+    from the given list of tuples containing feature strings. The first item in
+    each tuple is the phoneme and the second is the feature string.
+
+    '''
+
+    # Record the Levenshtein distance between each feature string and the
+    # target.
+    ranked_options = [(distance(target_feature_string, string[1]), string) for
+                      string in all_feature_strings]
+
+    # Sort based on distance between the strings.
+    ranked_options.sort(key=lambda x: x[0])
+
+    # Remove all options that don't have the current lowest distance
+    lowest = ranked_options[0][0]
+    lowest_options = [option for option in ranked_options if option[0] ==
+                      lowest]
+
+    # Sort by length of the segment, as we want the simplest possible segment.
+    lowest_options.sort(key=lambda x: len(x[1][0]))
+
+    # Return the first remaining segment
+    return lowest_options[0][1][0]
 
 
 def deparse_words(words, segments, diacritics):
