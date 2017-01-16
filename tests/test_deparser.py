999709ff09eb8f30f55e119389b02d8c947d5c2b
Cadel Watson <cadel@cadelwatson.com>
2017-01-16 16:29:03 +1100

    Add test for the deparser

---
+++ tests/test_deparser.py
@@ -0,0 +1,17 @@
+import sys
+import csv
+import os.path as path
+
+base_directory = path.dirname(path.dirname(path.abspath(__file__)))
+
+sys.path.append(path.join(base_directory, 'app'))
+
+from deparse import feature_string
+from segment import Segment
+
+
+def test_feature_string():
+    segment = Segment(['consonantal', 'voice', 'labial'],
+                      ['syllabic', 'stress', 'long', 'sonorant', 'continuant', 'delayedrelease', 'approximant', 'tap', 'trill', 'nasal', 'spreadglottis', 'constrictedglottis', 'round', 'labiodental', 'coronal', 'lateral', 'dorsal'])
+
+    assert feature_string(segment) == '---+-------+--+---000--00000'
