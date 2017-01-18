5aad0a67464ce6b8b73f42bd29be0775392a6216
Cadel Watson <cadel@cadelwatson.com>
2017-01-18 15:09:31 +1100

    Refactor deparser

--- app/deparse.py
+++ app/deparse.py
@@ -1,8 +1,5 @@
 from functools import partial
-
 from Levenshtein import distance
-
-import csv
 
 feature_order = ['syllabic',
                  'stress',
@@ -49,17 +46,18 @@ def feature_string(segment):
     return ''.join(features)
 
 
-def segment_match(all_feature_strings, target_feature_string):
-    '''Returns the best match for the segment denoted by the target feature string,
-    from the given list of tuples containing feature strings. The first item in
-    each tuple is the phoneme and the second is the feature string.
+def segment_match(feature_strings, target_segment):
+    '''Returns the best match for the IPA string of the given Segment, from the
+    given list of tuples containing feature strings. The first item in each
+    tuple is the phoneme and the second is the feature string.
 
     '''
+    target_feature_string = feature_string(target_segment)
 
     # Record the Levenshtein distance between each feature string and the
     # target.
     ranked_options = [(distance(target_feature_string, string[1]), string) for
-                      string in all_feature_strings]
+                      string in feature_strings]
 
     # Sort based on distance between the strings.
     ranked_options.sort(key=lambda x: x[0])
@@ -76,25 +74,16 @@ def segment_match(all_feature_strings, target_feature_string):
     return lowest_options[0][1][0]
 
 
-def deparse_segment(diacritic_translations, feature_strings, segment):
-    '''Deparses a segment into an IPA string, provided a dictionary of diacritic
-    feature-to-IPA translations and a list of feature strings.'''
-
-    # Get the base segment by matching with the feature string data
-    segment_string = segment_match(feature_string(segment),
-                                   feature_strings)
-
-
 def deparse_words(words, segments, feature_strings):
     '''Given a list of Words, return a list of IPA strings, one for each
     word.'''
 
-    # Partially apply the deparse_segment function to avoid repeated calls with
+    # Partially apply the segment_match function to avoid repeated calls with
     # the feature_strings object.
     deparse = partial(segment_match, feature_strings)
 
     # Deparse each segment in each word
-    word_strings = [''.join(deparse(feature_string(segment)) for segment in
-                            word.segments) for word in words]
+    word_strings = [''.join(deparse(segment) for segment in word.segments)
+                    for word in words]
 
     return word_strings
