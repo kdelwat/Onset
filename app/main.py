cde79e05941d68c27d1b0d6f1524432e08c5368e
Cadel Watson <cadel@cadelwatson.com>
2017-01-18 15:30:20 +1100

    Provide minimal parse/deparse example in main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,22 @@
 import csv
+import yaml
 
-import parser
-
-
-def load_segments(filename):
-    '''Load a segment feature matrix from a CSV file, returning a list of
-    dictionaries with information about each segment.
-
-    '''
-    with open(filename, 'r') as f:
-        return [segment for segment in csv.DictReader(f)]
+import parse
+import deparse
 
 
 def main():
-    print(load_segments('data/features.csv'))
+    with open('data/features.csv', 'r') as f:
+        segments = [segment for segment in csv.DictReader(f)]
+
+    with open('data/diacritics.yaml') as f:
+        diacritics = yaml.load(f)
+
+    with open('data/feature-strings-with-diacritics.csv') as f:
+        feature_strings = [line for line in csv.reader(f)]
+
+    words = parse.parse_words(['bːɒtl', 'b\u02D0ɒtl'], segments, diacritics)
+    print(deparse.deparse_words(words, segments, feature_strings))
 
 
 if __name__ == '__main__':
