a25dbd20e34ef8dd7b4015f40d3b4ad321f472f8
Cadel Watson <cadel@cadelwatson.com>
2017-01-22 16:16:07 +1100

    Add apply_rules stage to evolver

--- app/evolve.py
+++ app/evolve.py
@@ -7,6 +7,20 @@ def filter_rules(words, rules):
             if any([word.applicable(rule) for word in words])]
 
 
+def apply_rules(words, rules):
+    '''Given a list of words and a list of rules, return a list of tuples. The
+    first value of each tuple is the rule applied, while the second is the list
+    of words with that rule applied.
+
+    '''
+    results = []
+
+    for rule in rules:
+        results.append((rule, [word.apply_rule(rule) for word in words]))
+
+    return results
+
+
 def evolve(words, rules):
     '''Evolve a list of Words, returning the applied rule and the new list of
     Words.'''
