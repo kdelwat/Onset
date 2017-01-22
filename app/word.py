a69802ab166d072efc37507846b92961b6bf9438
Cadel Watson <cadel@cadelwatson.com>
2017-01-22 14:22:26 +1100

    Add applicable() method to Word to test if rules are applicable.

--- app/word.py
+++ app/word.py
@@ -1,17 +1,12 @@
 class Word:
     '''A representation of a word, containing multiple Segments.'''
 
-    def applicable(self, rule):
-
-        '''Returns True if the word meets all conditions of the given rule.'''
-
-        pass
     def __init__(self, segments):
         self.segments = segments
 
     @staticmethod
     def overlapping_chunks(segment_list, size):
-        '''Given a list, returns a list of lists, where each sublist has length
+        '''Given a list, returns a generator of lists, where each list has length
         size and overlaps with the previous list except for its final item.
 
         For example, [1, 2, 3, 4, 5] chunked with length 3 would give:
@@ -19,6 +14,45 @@ class Word:
             [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
 
         '''
-        return [segment_list[x: x + size] for x in range(0, len(segment_list) -
-                                                         size + 1)]
+        for x in range(0, len(segment_list) - size + 1):
+            yield tuple(segment_list[x:x + size])
 
+    def applicable(self, rule):
+        '''Returns True if any segment in the word meets all conditions of the
+        given rule.'''
+
+        # When before and after conditions are both present, check all three
+        if 'before' in rule and 'after' in rule:
+            chunks = self.overlapping_chunks(self.segments, 3)
+            for before, current, after in chunks:
+                if all([before.meets_conditions(rule['before']),
+                        current.meets_conditions(rule['conditions']),
+                        after.meets_conditions(rule['after'])]):
+                    return True
+
+        # When before conditions are present, check that and the current
+        # conditions
+        elif 'before' in rule:
+            chunks = self.overlapping_chunks(self.segments, 2)
+            for before, current in chunks:
+                if all([before.meets_conditions(rule['before']),
+                        current.meets_conditions(rule['conditions'])]):
+                    return True
+
+        # When after conditions are present, check that and the current
+        # conditions
+        elif 'after' in rule:
+            chunks = self.overlapping_chunks(self.segments, 2)
+            for current, after in chunks:
+                if all([current.meets_conditions(rule['conditions']),
+                        after.meets_conditions(rule['after'])]):
+                    return True
+
+        # Otherwise, just check each current segment
+        else:
+            for current in self.segments:
+                if current.meets_conditions(rule['conditions']):
+                    return True
+
+        # If none of the segments met the conditions, return False
+        return False
--- tests/test_word.py
+++ tests/test_word.py
@@ -1,3 +1,4 @@
+import yaml
 import sys
 import os.path as path
 
@@ -5,15 +6,59 @@ base_directory = path.dirname(path.dirname(path.abspath(__file__)))
 sys.path.append(path.join(base_directory, 'app'))
 
 from word import Word
+from segment import Segment
+
+# Load rules
+with open(path.join(base_directory, 'app', 'data', 'rules.yaml'), 'r') as f:
+    rules = yaml.load(f)
 
 
 def test_overlapping_chunks():
     word = Word([])
-    assert word.overlapping_chunks([], 3) == []
-    assert word.overlapping_chunks([1, 2, 3, 4, 5], 2) == [[1, 2],
-                                                           [2, 3],
-                                                           [3, 4],
-                                                           [4, 5]]
-    assert word.overlapping_chunks([1, 2, 3, 4, 5], 3) == [[1, 2, 3],
-                                                           [2, 3, 4],
-                                                           [3, 4, 5]]
+    assert list(word.overlapping_chunks([], 3)) == []
+    assert list(word.overlapping_chunks([1, 2, 3, 4, 5], 2)) == [(1, 2),
+                                                                 (2, 3),
+                                                                 (3, 4),
+                                                                 (4, 5)]
+    assert list(word.overlapping_chunks([1, 2, 3, 4, 5], 3)) == [(1, 2, 3),
+                                                                 (2, 3, 4),
+                                                                 (3, 4, 5)]
+
+
+def test_applicable():
+
+    rule_length_one = {'applies': {'positive': ['nasal']},
+                       'conditions': {'negative': ['nasal'],
+                                      'positive': ['syllabic']},
+                       'name': 'nasalization'}
+
+    rule_length_two = {'applies': {'positive': ['nasal']},
+                       'before': {'positive': ['nasal']},
+                       'conditions': {'negative': ['nasal'],
+                                      'positive': ['syllabic']},
+                       'name': 'nasalization'}
+
+    rule_length_three = {'applies': {'positive': ['nasal']},
+                         'before': {'positive': ['nasal']},
+                         'after': {'negative': ['high']},
+                         'conditions': {'negative': ['nasal'],
+                                        'positive': ['syllabic']},
+                         'name': 'nasalization'}
+
+    applicable_word = Word([Segment(['consonantal'], ['tonal']),
+                            Segment(['nasal'], ['syllabic']),
+                            Segment(['syllabic'], ['nasal']),
+                            Segment(['sonorant'], ['high'])])
+
+    inapplicable_word = Word([Segment(['consonantal'], ['tonal']),
+                              Segment([], ['low']),
+                              Segment(['syllabic', 'nasal'], []),
+                              Segment(['high'], ['sonorant'])])
+
+    assert applicable_word.applicable(rule_length_one)
+    assert applicable_word.applicable(rule_length_two)
+    assert applicable_word.applicable(rule_length_three)
+
+    assert not inapplicable_word.applicable(rule_length_one)
+    assert not inapplicable_word.applicable(rule_length_two)
+    assert not inapplicable_word.applicable(rule_length_three)
