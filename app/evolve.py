0b8582f32c9fe534b8222aa529987367ff2d05f5
Cadel Watson <cadel@cadelwatson.com>
2017-01-24 10:05:09 +1100

    Complete evolve step functions

--- app/evolve.py
+++ app/evolve.py
@@ -1,3 +1,8 @@
+from collections import defaultdict
+
+import metrics
+
+
 def filter_rules(words, rules):
     '''Given a list of words and a list of rules, return only those rules which are
     applicable to the given words.
@@ -21,9 +26,40 @@ def apply_rules(words, rules):
     return results
 
 
+def average_metric_value(words, metric):
+    '''Apply the metric function to each word in words, returning the average
+    result.'''
+    return sum(map(metric, words)) / len(words)
+
+
+def optimal_rule_pair(rule_pairs, metric, optimisation_function=min):
+    '''Given a list of tuples of form (rule, words), rank each pair according to
+    the given metric function. Return the rule and words which are first when
+    ranked using the optimisation function.
+
+    '''
+    ranked_pairs = defaultdict(list)
+
+    # Add each pair to the dictionary with the key given by the average metric
+    # value of the words.
+    for rule, words in rule_pairs:
+        ranked_pairs[average_metric_value(words, metric)].append((rule, words))
+
+    # If there is more than one pair for the best metric value, return the
+    # first
+    return ranked_pairs[optimisation_function(ranked_pairs.keys())][0]
+
+
 def evolve(words, rules):
     '''Evolve a list of Words, returning the applied rule and the new list of
     Words.'''
-    pass
 
+    rules = filter_rules(words, rules)
 
+    # If there are no applicable rules, inform the evolution controller
+    if len(rules) == 0:
+        raise StopIteration
+
+    rule_pairs = apply_rules(words, rules)
+
+    return optimal_rule_pair(rule_pairs, metrics.phonetic_product)
