21541a4a1b2fcdfaa4bb7d2fbebf7db702eebacc
Cadel Watson <cadel@cadelwatson.com>
2017-01-24 10:05:21 +1100

    Create basic flow of evolution process.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,28 @@ import csv
 import yaml
 
 import parse
+import evolve
 import deparse
+
+
+def evolve_words(words, available_rules, generations=5):
+    '''Evolves the given list of words according to the given list of rules, for a
+    number of generations. If no more applicable rules are available, the evolution
+    will stop early. Returns the evolved list of words and a list of rule which
+    were applied.
+
+    '''
+    applied_rules = []
+
+    try:
+        for _ in range(generations):
+            rule, words = evolve.evolve(words, available_rules)
+            applied_rules.append(rule)
+    # StopIteration is raised when there are no more applicable rules
+    except StopIteration:
+        return words, applied_rules
+
+    return words, applied_rules
 
 
 def main():
@@ -12,11 +33,22 @@ def main():
     with open('data/diacritics.yaml') as f:
         diacritics = yaml.load(f)
 
+    with open('data/rules.yaml') as f:
+        rules = yaml.load(f)
+
     with open('data/feature-strings-with-diacritics.csv') as f:
         feature_strings = [line for line in csv.reader(f)]
 
-    words = parse.parse_words(['bːɒtl', 'b\u02D0ɒtl'], segments, diacritics)
-    print(deparse.deparse_words(words, segments, feature_strings))
+    word_strings = ['mːɒtl', 'b\u02D0ɒtl']
+
+    words = parse.parse_words(word_strings, segments, diacritics)
+
+    evolved_words, applied_rules = evolve_words(words, rules)
+
+    deparsed = deparse.deparse_words(evolved_words, segments, feature_strings)
+
+    for word, evolved_word in zip(word_strings, deparsed):
+        print('{0} -> {1}'.format(word, evolved_word))
 
 
 if __name__ == '__main__':
