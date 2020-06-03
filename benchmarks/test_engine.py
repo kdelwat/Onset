d1667dd17c23d907d2f51a6cb545aa61bdfafa6d
Cadel Watson <cadel@cadelwatson.com>
2020-06-03 17:50:44 +1000

    Create benchmark for engine

---
+++ benchmarks/test_engine.py
@@ -0,0 +1,67 @@
+import engine.engine as engine
+
+
+def test_engine_fast(benchmark):
+    words, rules = benchmark(
+        engine.run_engine, ["aba", "bːɒtl", "b\u02D0ɒtl", "b\u02D0ɒbtdolie"]
+    )
+
+    assert words == ["ab͡βaː", "bːɒːdl", "bːɒːdl", "bːɒːbddoːliːe"]
+    assert rules == [
+        {
+            "name": "Gemination",
+            "description": "Plosives become lengthened between vowels.",
+            "before": {"positive": ["syllabic"]},
+            "after": {"positive": ["syllabic"]},
+            "conditions": {
+                "positive": ["consonantal"],
+                "negative": ["continuant", "delayedrelease", "sonorant", "long"],
+            },
+            "applies": {"positive": ["long"]},
+        },
+        {
+            "name": "Affrication",
+            "description": "Plosives become affricates between vowels.",
+            "before": {"positive": ["syllabic"]},
+            "after": {"positive": ["syllabic"]},
+            "conditions": {
+                "positive": ["consonantal"],
+                "negative": ["continuant", "delayedrelease", "sonorant"],
+            },
+            "applies": {"positive": ["delayedrelease"]},
+        },
+        {
+            "name": "Degemination",
+            "description": "Geminated consonants are shortened after vowels.",
+            "before": {"positive": ["syllabic"]},
+            "conditions": {"positive": ["long"]},
+            "applies": {"negative": ["long"]},
+        },
+        {
+            "name": "Lengthening",
+            "description": "Vowels are lengthened after voiced consonants.",
+            "conditions": {"positive": ["syllabic"], "negative": ["long"]},
+            "before": {"positive": ["voice", "consonantal"]},
+            "applies": {"positive": ["long"]},
+        },
+        {
+            "name": "Voicing",
+            "description": "Consonants become voiced after voiced segments.",
+            "before": {"positive": ["voice"]},
+            "conditions": {"negative": ["voice"]},
+            "applies": {"positive": ["voice"]},
+        },
+    ]
+
+
+def test_engine_slow(benchmark):
+    with open("benchmarks/benchmark_input.txt", "r") as words_in:
+        input_words = [word.strip() for word in words_in]
+
+    words, rules = benchmark(engine.run_engine, input_words, generations=50)
+
+    with open("benchmark_expected_words.txt") as words_out:
+        words_out.writelines(words)
+
+    with open("benchmark_expected_rules.text") as rules_out:
+        rules_out.write(rules)
---
+++ scripts/generate_benchmark_input.py
@@ -0,0 +1,49 @@
+# Use like python generate_benchmark_input.py n benchmark_input.txt
+# Generates a file containing one IPA word per line, totalling n words,
+# with each word randomly generated
+import csv
+import sys
+import os.path as path
+import random
+
+base_directory = path.dirname(path.dirname(path.abspath(__file__)))
+sys.path.append(path.join(base_directory, "engine"))
+
+MIN_WORD_LENGTH = 1
+MAX_WORD_LENGTH = 10
+
+
+def load_feature_strings(filename):
+    """Load a feature string list from a CSV file, returning a list of
+  lists where the first item in each is the IPA string and the
+  second is the corresponding feature string.
+
+  """
+    with open(filename, "r") as f:
+        return [line for line in csv.reader(f)]
+
+
+def generate_random_word(phonemes):
+    l = random.randrange(MIN_WORD_LENGTH, MAX_WORD_LENGTH + 1)
+
+    return "".join([random.choice(phonemes) for _ in range(l)])
+
+
+def main():
+    n = int(sys.argv[1])
+    output_filename = sys.argv[2]
+
+    phonemes = [
+        feature_string[0]
+        for feature_string in load_feature_strings(
+            path.join(base_directory, "engine", "data", "features.csv")
+        )
+    ]
+
+    with open(output_filename, "w") as out:
+        for _ in range(n):
+            out.write(generate_random_word(phonemes) + "\n")
+
+
+if __name__ == "__main__":
+    main()
